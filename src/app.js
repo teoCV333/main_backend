62d88d3ab7e1c46071bf007900606e6487c460e9
MCVdev <mateocarvajalvergara333@gmail.com>
2025-07-08 18:23:03 -0500

    commit 3

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://eliminacuotaonline.lat ");
+  res.header("Access-Control-Allow-Origin", "https://eliminacuotaonline.lat");
   res.header("X-Content-Type-Options", "nosniff");
   res.header("X-Frame-opts", "DENY");
   res.header("X-XSS-Protection", "1; mode=block");
