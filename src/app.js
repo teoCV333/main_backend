c8a5d2024f7f8b3aae4cee56d3a2a0dcee9629c1
MCVdev <mateocarvajalvergara333@gmail.com>
2025-07-17 15:07:57 -0500

    new configuration

--- src/app.js
+++ src/app.js
@@ -7,12 +7,28 @@ import cors from 'cors';
 const app = express();
 
 app.use(cors({
-    origin: 'https://eliminacuotaonline.lat', // tu frontend
+    origin: 'https://removercuotaonline.com', // tu frontend
     credentials: true
 }));
 
+const botUserAgents = [
+  /bot/i, /crawl/i, /slurp/i, /spider/i, /Yandex/i, /Bingbot/i, /Googlebot/i
+];
+
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://eliminacuotaonline.lat");
+  const userAgent = req.headers['user-agent'] || '';
+  const isBot = botUserAgents.some(pattern => pattern.test(userAgent));
+
+  if (isBot) {
+    console.warn('Bot detectado:', userAgent);
+    return res.status(403).send('Acceso denegado');
+  }
+
+  next();
+});
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://removercuotaonline.com");
   res.header("X-Content-Type-Options", "nosniff");
   res.header("X-Frame-opts", "DENY");
   res.header("X-XSS-Protection", "1; mode=block");
